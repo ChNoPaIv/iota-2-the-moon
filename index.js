b6bafcf7525a0ea5dca12d3b2827f188548ee616
Christian Normando Palec Iversen <149884@stud.hvl.no>
2018-05-12 16:17:31 +0200

    EOS added

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const bot = new commando.Client();
 
 bot.registry.registerGroup('random', 'Random');
 bot.registry.registerGroup('iota', 'IOTA');
+bot.registry.registerGroup('eos', 'EOS');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
-bot.login(process.env.ACCESS_TOKEN);
+bot.login('NDQ0NTM2ODEzNzgxMjU0MTQ0.Ddh1nA.x6LDRxehMjUYI-KnR6D1LIxzEVk');
 
