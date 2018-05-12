35780b095f2b3694b458e67570eceb219ef891dd
Christian Normando Palec Iversen <149884@stud.hvl.no>
2018-05-12 15:03:45 +0200

    DAPLY

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const commando = require('discord.js-commando');
+const bot = new commando.Client();
+
+bot.registry.registerGroup('random', 'Random');
+bot.registry.registerGroup('iota', 'IOTA');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + "/commands");
+
+bot.login(process.env.ACCESS_TOKEN);
+
