346af89e8eadf65e50e0624f0a97608f657cdc9d
Christian Normando Palec Iversen <christian@iversen.no>
2018-05-14 17:38:04 +0200

    dsafasd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new commando.Client();
 bot.registry.registerGroup('random', 'Random');
 bot.registry.registerGroup('eos', 'EOS');
 bot.registry.registerGroup('iota', 'IOTA');
-bot.registry.registerDefaults();
+bot.registry.registerGroup('btc', 'BTC');
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
 bot.login(process.env.ACCESS_TOKEN);
